b4c634fea58abe29bd7069b5bcc1d7244342864f
shohagcsediu <shohagcsediu@gmail.com>
2019-07-09 21:10:17 +0600

    first commit

---
+++ app/src/main/java/com/shohagapp/waveanimation/MainActivity.java
@@ -0,0 +1,39 @@
+package com.shohagapp.waveanimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.scwang.wave.MultiWaveHeader;
+
+public class MainActivity extends AppCompatActivity {
+
+    MultiWaveHeader waveHeader, waveFooter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        waveHeader = findViewById(R.id.wave_header);
+        waveFooter = findViewById(R.id.wave_footer);
+
+        waveHeader.setVelocity(1);
+        waveHeader.setProgress(1);
+        waveHeader.isRunning();
+        waveHeader.setGradientAngle(45);
+        waveHeader.setWaveHeight(40);
+        waveHeader.setStartColor(Color.RED);
+        waveHeader.setCloseColor(Color.CYAN);
+
+        waveFooter.setVelocity(1);
+        waveFooter.setProgress(1);
+        waveFooter.isRunning();
+        waveFooter.setGradientAngle(45);
+        waveFooter.setWaveHeight(40);
+        waveFooter.setStartColor(Color.MAGENTA);
+        waveFooter.setCloseColor(Color.YELLOW);
+
+    }
+}
